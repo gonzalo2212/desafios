07e3b41bd3590577f8dd78330e8b4881c237d7d4
Gonzalo <gonzalovargas221@gmail.com>
2020-11-22 19:55:08 -0300

    Segunda version snail

--- snail.py
+++ snail.py
@@ -1,32 +1,22 @@
 def snail(snail_map):
     size = len(snail_map[0])
-    col = row = 0
-    reversa = False
+    aux = 0
     res = list()
     while size > 0:
-        if not reversa:
-            if col < size-1:
-                res.append(snail_map[row][col])
-                col += 1
-            elif row < size:
-                res.append(snail_map[row][col])
-                row += 1
-            else:
-                size = size-1
-                row -= 1
-                col -= 1
-                reversa = True
-        else:
-            if col >= len(snail_map)-size:
-                res.append(snail_map[row][col])
-                col -= 1
-            elif row >= len(snail_map)-size:
-                res.append(snail_map[row][col])
-                row -= 1
-            else:
-                col += 1
-                row += 1
-                reversa = False
+        # Top Row
+        res.extend(snail_map[aux][aux:size-1])
+        # Rigth Col
+        res.extend([r[size-1] for r in snail_map[aux:size-1]])
+        # Bottom Row
+        res.extend(snail_map[size-1][::-1][aux:size-1])
+        # Left Col
+        res.extend([r[aux] for r in snail_map[::-1][aux:size-1]])
+        # Center
+        if aux == size-1:
+            res.append(snail_map[aux][size-1])
+            break
+        size -= 1
+        aux += 1
     
     return res
 
@@ -35,7 +25,8 @@ def snail(snail_map):
 
 array = [[1,2,3],
          [8,9,4],
-         [7,6,5]]
+         [7,6,5]]    
+
 expected = [1,2,3,4,5,6,7,8,9]
 print(snail(array))
 assert (snail(array) == expected)
